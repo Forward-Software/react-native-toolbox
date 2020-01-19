4c971e1c484ea7a001ed77aac230f04cd38f6e5a
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-01-19 21:27:09 +0100

    feat(dotenv): Add script to manage 'dotenv' files

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,2 +1,3 @@
+export { default as EnvironmentManager } from './environment-manager';
 export { default as IconGenerator } from './icon-generator';
 export { default as SplashGenerator } from './splash-generator';
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 /*
-* Copyright (c) 2018 Mattia Panzeri <mattia.panzeri93@gmail.com>
-* 
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. 
-*/
+ * Copyright (c) 2018 Mattia Panzeri <mattia.panzeri93@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 import program from 'commander';
 
-import { IconGenerator, SplashGenerator } from './commands';
+import { EnvironmentManager, IconGenerator, SplashGenerator } from './commands';
 
-program.description(process.env.PKG_DESCRIPTION).version(process.env.PKG_VERSION);
+program
+  .description(process.env.PKG_DESCRIPTION)
+  .version(process.env.PKG_VERSION);
 
 program.command('icons').action(cmd => {
   new IconGenerator().generateIcons();
@@ -19,5 +21,14 @@ program.command('icons').action(cmd => {
 program.command('splash').action(cmd => {
   new SplashGenerator().generateSplashscreens();
 });
+
+program
+  .command('dotenv <environmentName>')
+  .description(
+    'Manage .env files for react-native-dotenv for a specific environment (development, production, etc...)',
+  )
+  .action(environmentName => {
+    EnvironmentManager.setupEnvironment({ envName: environmentName });
+  });
 
 program.parse(process.argv);
